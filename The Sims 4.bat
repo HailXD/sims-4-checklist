dbfa13897af37263d0418233b54c60ceea580cf1
HailXD <jyealc@gmail.com>
2025-12-28 00:56:00 +0800

    Update Sims 4.bat

--- The Sims 4.bat
+++ The Sims 4.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 
-set "TS4_EXE=C:\Program Files (x86)\Steam\steamapps\common\The Sims 4\Game\Bin\TS4_x64.exe"
+set STEAM_EXE="C:\Program Files (x86)\Steam\steam.exe"
 
-set ARGS=-disablepacks:EP06,EP08,EP12,EP14,GP03,GP04,GP06,GP07,GP08,GP09,GP10,GP12
+set ARGS=-applaunch 1222670 -console -disablepacks:EP06,EP11,EP14,EP17,EP19,GP04,GP06,GP07,GP08,GP09,GP10,GP12
 
-start "" "%TS4_EXE%" %ARGS%
+start "" %STEAM_EXE% %ARGS%
 
 endlocal
